bedcbacc1b8a976afa25b3cd72523bb398911f9d
That Guy Vic <vicsergeevrov@icloud.com>
2023-06-04 17:19:34 +0300

    June 4th supplemental
    collections

--- swiftbook_recall.playground/Contents.swift
+++ swiftbook_recall.playground/Contents.swift
@@ -230,4 +230,215 @@ let defaultColor = ".red"
 var userColor: String?
 var colorToUse = userColor ?? defaultColor
 
+// range operators
+// closed range operator
+// a...b from a to b including b, iterable, e.g. for in loop
+for i in 1...5 {
+    print("\(i) times five is \(i*5)")
+}
+
+// half-opened range
+// a..<b from a to b not including b
+for i in 1..<8 {
+    print("\(i) times 4 is \(i*4)")
+}
+
+// one-sided range
+// a... continuing as far as possible in one direction (+/-)
+let rangeArr = [1,2,3,4,5,6,7]
+for i in rangeArr[3...] {
+    print(i) // 4,5,6,7
+}
+
+// impossible to iterate thru range without initial item, because compiler can't understand where to beign
+let unknownBeginning = ...5 // upperBound
+unknownBeginning.contains(-8) // true
+unknownBeginning.contains(7) // false
+unknownBeginning.contains(2) // true
+
+// Logical operators
+// && logical AND - true only if both condition are true
+// || logical OR - true if either one of condition is true
+// !a - local NOT - toggle condition !false = true
+if 3 > 2 && 5 < 7 {
+    if 2 > 3 || 9 > 4 {
+        print("some result")
+    }
+}
+// sequences
+if (1 < 2 || 4 > 5) && (2 != 3 && 6 < 9) {
+    print("some result")
+}
+
+
+// types of value
+
+// String to create use String literals - sequence of characters(char) surrounded by double quotes "String"
+let literallyString: String = "This is String literals"
+// multiline strings
+let multiline = """
+    first four spaces are multiline string scope
+    they are must be empty
+    and each new line must be started after four whitespaces
+    String - value type
+    you can loop thru strings
+"""
+print(multiline)
+
+// looping
+var loopThru = "loop"
+for char in loopThru {
+    print(char)
+    // l
+    // o
+    // o
+    // p
+}
+// string concatination
+var concatination = literallyString + multiline + " Hello, World!"
+let exclamation: Character = "!"
+print(concatination)
+
+print(loopThru)
+// accessing char in string by index
+let charFromString = loopThru[loopThru.index(loopThru.startIndex, offsetBy: 2)] // accessing index 2
+// direct access
+extension StringProtocol {
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
+    }
+}
+let directChar = loopThru[3]
+
+// insert & remove
+loopThru.append(exclamation)
+loopThru.removeLast()
+var hello = "hello"
+hello.insert(contentsOf: ", world", at: hello.endIndex) // hello, world
+hello.remove(at: hello.index(hello.startIndex, offsetBy: 5))
+// remove at index-start from index 5 in hello string variable
+
+// Collections
+// array, set, dictionary
+// arrays - ordered list of contents of the same type
+let immutableArray: Array<Int> = [1,2,3,4]
+var mutableArray: [Int] = [4,5,6,7]
+let shortHandArray = [8,9,10,11] // preferable
+let twinsDoubles = Array(repeating: 3.14, count: 5) // (repeating: what, count: how many items)
+print(twinsDoubles)
+// array concatenation, ONLY THE SAME TYPE
+var twoArrays = immutableArray + mutableArray
+print(twoArrays)
+// accessing & modifying
+twoArrays.count
+twoArrays.append(8)
+twoArrays.isEmpty // false
+twoArrays.remove(at: 3)
+twoArrays.removeLast()
+twoArrays.removeFirst()
+// direct addition
+twoArrays.insert(34, at: 0) // add 34 at index 0
+// loop thru
+for i in twoArrays {
+    print(i)
+}
+
+// show index/value
+for (index, value) in twoArrays.enumerated() { // must use enumerated method
+    print("the \(value) in twoArrays Array has index \(index)")
+}
+print(twoArrays.sorted())
+
+//set[] - stores unordered list of elements of the same type, the same elements will be ignored
+let ignoredItems: Set<Int> = [1,1,2,2,2,3,4,4,4,5,6,] // 1,2,3,4,5,6
+print(ignoredItems.sorted())
+// same mothods can be used
+var someSet: Set<Int> = [1,2,3,4,5]
+someSet.count
+someSet.isEmpty
+someSet.contains(4) // true
+someSet.insert(45)
+someSet.contains(4) // true
+// looping
+for i in someSet {
+    print(i)
+}
+
+// operations with set
+
+// intersection - method create new set with elements common for both sets
+let setOne: Set<Int> = [1,2,3,4,5]
+let setTwo: Set<Int> = [1,2,3,4,6]
+let setThree: Set<Int> = [1,2,3,4]
+let setFour: Set<Int> = [7,8,9]
+let intersectionSet = setOne.intersection(setTwo)
+print(intersectionSet) // 1,2,3,4 common for both
+
+// symmetric difference - method create new set without common elements and only elements different to each other letf
+let difference = setOne.symmetricDifference(setTwo) // 5,6 - uncommon for both
+print(difference)
+
+// union - method create new set with elements from both sets, twins will be erased
+let union = setOne.union(setTwo)
+print(union)
+
+// subtracting - method create new set of values that only setOne has
+let subtracting = setOne.subtracting(setTwo)
+print(subtracting)
+
+// subSet & superSet
+// check with == weither both sets have same elements
+let superSetOf = setOne.isSuperset(of: setThree)
+let subSetOf = setThree.isSubset(of: setOne)
+let subSetOf1 = setOne.isSubset(of: setThree)
+let superSetOf1 = setThree.isSuperset(of: setOne)
+print(superSetOf, superSetOf1, subSetOf, subSetOf1)
+
+// disjoint - return weither the both sets hava uncommon elemets
+let disjointWith = setOne.isDisjoint(with: setFour)
+print(disjointWith) // true
+
+// Dictionaries - values associated with each other, [key: value], unordered
+let fullDictionaryAnnotation: Dictionary<String, String> = ["PRG":"Praha", "ARN":"Arlando", "ROV":"Rostov-on-Don"]
+let shortHandDict: [String: Int] = [:]
+var someDict: [String: String] = fullDictionaryAnnotation
+print(someDict)
+// add new item
+someDict["LAX"] = "Los Angeles"
+// dictionary you add in->["NEW KEY"] = "NEW VALUE"
+someDict["MIA"] = "miami, florida"
+print(someDict)
+// change old value
+someDict["PRG"] = "Prague"
+print(someDict)
+// update old value via method
+someDict.updateValue("Miami", forKey: "MIA")
+someDict["VKO"] = "Vnukovo"
+someDict["LED"] = "Pulkovo"
+print(someDict)
+// erase pair by key
+someDict["VKO"] = nil
+print(someDict)
+someDict.removeValue(forKey: "LED")
+print(someDict)
+
+// methods
+someDict.count // bool
+someDict.isEmpty // bool
+print(someDict.keys) // return only keys
+someDict.values // return values
+
+// loop thru
+for key in someDict.keys {
+    print(key)
+}
+for value in someDict.values {
+    print(value)
+}
+
+for (key, value) in someDict {
+    print("Airport \(key) is \(value)")
+}
+
+// control flow
 
